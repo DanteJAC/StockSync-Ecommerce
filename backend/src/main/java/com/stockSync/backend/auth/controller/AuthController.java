77e1f80a3b03669091118490002d4322c10429a0
Dante <jacobo.bustos.22@gmail.com>
2026-05-20 13:04:02 -0400

    feat: Correccion direcciones api en client.js

--- backend/src/main/java/com/stockSync/backend/auth/controller/AuthController.java
+++ backend/src/main/java/com/stockSync/backend/auth/controller/AuthController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping
 @RequiredArgsConstructor
-@CrossOrigin(origins = "https://stocksync-ecommerce.onrender.com")
 public class AuthController {
 
     private final AuthService authService;
